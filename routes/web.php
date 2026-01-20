b8c5ac4bded749de2a63dce3347d44d11f9b93bd
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-01-20 22:10:24 +0600

    Added a maney route files there

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FrontController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get( '/', [FrontController::class, 'index'] )->name( 'home' );
 Route::get( '/about', [FrontController::class, 'about'] )->name( 'about' );
+
+/**
+ * User Routes
+ */
+
+Route::controller( UserController::class )->group( function () {
+    Route::get( '/user/login', 'login' )->name( 'user.login' );
+} );
+
+/**
+ * Admin Routes
+ */
+
+Route::controller( AdminController::class )->group( function () {
+    Route::get( '/admin/login', 'adminLogin' )->name( 'admin.login' );
+    Route::get( '/admin/dashboard', 'dashboard' )->name( 'admin.dashboard' );
+} );
