18d149a6b85f5ced5eb68c68e9a86cda4fda2caa
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-01-30 20:07:51 +0600

    Add a admin login and registar menu

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -3,9 +3,10 @@
         <div class="logo" onclick="showPage('home')">YourBrand</div>
         <nav>
             <ul>
-                <li><a onclick="showPage('home')">Home</a></li>
-                <li><a onclick="showPage('login')">Login</a></li>
-                <li><a onclick="showPage('register')">Register</a></li>
+               <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ route('user.login') }}">Login</a></li>
+                  <li><a href="{{ route('admin.login') }}">Admin Login</a></li>
+                <li><a href="{{ route('user.register') }}">Register</a></li>
             </ul>
         </nav>
     </div>
