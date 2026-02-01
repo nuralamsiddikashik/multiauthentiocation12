d0075d73badb8cb90f0cbdf2129979703577f832
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-01 20:18:55 +0600

    Change home blade file location

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller {
 
     public function home() {
-        return view( 'front.home' );
+        return view( 'home' );
     }
 }
