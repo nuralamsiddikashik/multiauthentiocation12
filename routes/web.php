42b6da723f6fcd971938afe3345c212add8e9eb0
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-04 21:33:26 +0600

    added a api routes

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,13 @@ use Illuminate\Support\Facades\Route;
  * Frontend Routes
  */
 Route::get( '/', [HomeController::class, 'home'] )->name( 'home' );
+
+Route::get( '/test-bdapi', function () {
+
+    $res = Http::withHeaders( [
+        'X-RapidAPI-Key'  => config( 'services.bdapi.key' ),
+        'X-RapidAPI-Host' => config( 'services.bdapi.host' ),
+    ] )->get( 'https://bdapis.com/api/v1.2/division/rangpur' );
+
+    return $res->json();
+} );
