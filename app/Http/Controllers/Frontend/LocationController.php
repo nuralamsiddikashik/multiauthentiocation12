05c4d052a745164eb2802c77a79bdee56b9fb7e2
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-09 22:03:29 +0600

    Delete a previous code

--- app/Http/Controllers/Frontend/LocationController.php
+++ app/Http/Controllers/Frontend/LocationController.php
@@ -6,21 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
 
 class LocationController extends Controller {
-    // public function divisions() {
-    //     return Division::orderBy( 'name' )->get();
-    // }
-
-    // public function districts( $divisionId ) {
-    //     return District::where( 'division_id', $divisionId )
-    //         ->orderBy( 'name' )
-    //         ->get();
-    // }
-
-    // public function upazilas( $districtId ) {
-    //     return Upazila::where( 'district_id', $districtId )
-    //         ->orderBy( 'name' )
-    //         ->get();
-    // }
 
     public function divisions() {
         return Http::get( 'https://bdapis.com/api/v1.2/divisions' )->json();
