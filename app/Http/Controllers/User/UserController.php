c1d9d7bb9e758ca924089b55baac28aa0f266c0f
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-11 16:12:06 +0600

    add new create and construct method

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\User\UserAuthRepositoryInterface;
 
 class UserController extends Controller {
+
+    public function __construct(
+        private UserAuthRepositoryInterface $userAuthRepo ) {
+    }
+
     public function create() {
         return view( 'auth.user-registration' );
     }
-
-    
 }
