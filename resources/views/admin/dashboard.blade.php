cad9c8e560b2994053e62e73b132b35535453d3f
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-13 10:02:17 +0600

    Remove logout form blade file

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <h1>Hello Dashboard</h1>
 
-    <form method="POST" action="{{ route('admin.logout') }}">
+    {{-- <form method="POST" action="{{ route('admin.logout') }}">
         @csrf
         <button type="submit">Logout</button>
-    </form>
+    </form> --}}
 @endsection
