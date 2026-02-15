0a74b0287cf4b95d93ed30d6f7695c03e58f1145
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-15 20:51:08 +0600

    Main js file comment

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -19,7 +19,7 @@
     </div>  
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/main.js') }}"></script> --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
  @if (session('toast'))
 <script>
