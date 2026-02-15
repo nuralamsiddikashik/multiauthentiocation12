5fc3d451ed07a49a344db657567473506135f910
nuralamsiddikashik <nuralamsiddikashik@gmail.com>
2026-02-15 20:52:17 +0600

    Add a routes files

--- routes/user.php
+++ routes/user.php
@@ -3,7 +3,14 @@
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware( ['user'] )->group( function () {
+    Route::get( '/users/dashboard', [UserController::class, 'dashboard'] )->name( 'user.dashboard' );
+} );
+
 Route::controller( UserController::class )->group( function () {
-    Route::get( '/users/create', 'create' );
+    Route::get( '/users/create', 'create' )->name( 'user.register.create' );
     Route::post( '/users', 'store' )->name( 'user.store' );
+
+    Route::get( '/users/login', 'login' )->name( 'user.login' );
+    Route::post( '/user/submit', 'userLoginStore' )->name( 'user.login.store' );
 } );
